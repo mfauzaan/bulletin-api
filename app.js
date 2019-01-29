b89a20401d8adec923e90d408303bf93f1783e4f
Fauzaan <fauzaan@kodefly.com>
2019-01-30 00:13:51 +0500

    added generic error msg

--- app.js
+++ app.js
@@ -23,17 +23,24 @@ app.use('/', indexRouter);
 app.use(expressValidator())
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500).json({ message: err.message });
+  res.status(err.status || 500).json({
+    "errors": [
+      {
+        "code": "ACTION_FAILED",
+        "message": err.message,
+      }
+    ]
+  });
 });
 
 module.exports = app;
